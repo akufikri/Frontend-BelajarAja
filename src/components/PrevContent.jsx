e0f3af9b736551e675ae1a892110c08563556c03
fikrinurhakim911 <fikrinurhakim911@gmail.com>
2024-02-17 00:45:31 +0000

    create: add idx dev & component preview content

---
+++ src/components/PrevContent.jsx
@@ -0,0 +1,10 @@
+const PreviewContent = () => {
+    return(
+        <>
+            <div className=''>
+                <h1>Ini preview content</h1>
+            </div>
+        </>
+    )
+}
+export default PreviewContent;
--- src/page/Beranda.jsx
+++ src/page/Beranda.jsx
@@ -1,11 +1,13 @@
 import CardInfo from "../components/CardInfo";
 import HeroSections from "../components/HeroSections";
+import PreviewContent from "../components/PrevContent";
 
 const Beranda = () => {
       return (
             <div className="pt-20">
                   <HeroSections />
                   <CardInfo />
+                  <PreviewContent/>
             </div>
       )
 }
