7cb6c28aad5581795541ee1af44c5a17b0f9527e
Fikri Nurhakim <108182945+akufikri@users.noreply.github.com>
2024-03-14 19:29:53 +0700

    create: added analytic page view

--- src/App.jsx
+++ src/App.jsx
@@ -16,101 +16,103 @@ import RegisterMentor from './page/RegisterMentor';
 import Dashboard from './page/mentors/Dashboard';
 import Breadcrumbs from './components/Breadcrumbs';
 import NavMentor from './components/NavMentor';
+import { Analytics } from "@vercel/analytics/react"
 
 
 export default function App() {
   const { user } = useAuthContext()
   return (
     <div className='h-full'>
+      <Analytics>
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Nav />
+              <Beranda />
 
-      <Routes>
-        <Route path="/" element={
-          <>
-            <Nav />
-            <Beranda />
+            </>
+          } ></Route>
+          <Route path='/kelas' element={
+            <>
+              <Nav />
+              <Course />
+            </>
+          }></Route>
+          <Route path='/kelas/:id' element={
+            <>
+              <Nav />
+              <DetailCourse />
 
-          </>
-        } ></Route>
-        <Route path='/kelas' element={
-          <>
-            <Nav />
-            <Course />
-          </>
-        }></Route>
-        <Route path='/kelas/:id' element={
-          <>
-            <Nav />
-            <DetailCourse />
+            </>
+          }></Route>
+          <Route path='/user/settings' element={
+            <>
+              <Nav />
+              <AccountSettings />
 
-          </>
-        }></Route>
-        <Route path='/user/settings' element={
-          <>
-            <Nav />
-            <AccountSettings />
+            </>
+          }></Route>
+          <Route path='/signup' element={
+            <>
+              <Nav />
+              {!user ? <Register /> : <Navigate to='/' />}
 
-          </>
-        }></Route>
-        <Route path='/signup' element={
-          <>
-            <Nav />
-            {!user ? <Register /> : <Navigate to='/' />}
+            </>
+          }></Route>
+          <Route path='/login' element={
+            <>
+              <Nav />
+              {!user ? <Login /> : <Navigate to="/" />}
+            </>
+          }></Route>
+          <Route path='/mengajar/register' element={
+            <>
+              <Nav />
+              <RegisterMentor />
+            </>
+          }>
+          </Route>
 
-          </>
-        }></Route>
-        <Route path='/login' element={
-          <>
-            <Nav />
-            {!user ? <Login /> : <Navigate to="/" />}
-          </>
-        }></Route>
-        <Route path='/mengajar/register' element={
-          <>
-            <Nav />
-            <RegisterMentor />
-          </>
-        }>
-        </Route>
+          {/* Mentor Route */}
+          <Route path='/mentor/dashboard' element={
+            <>
+              {user && <Sidebars />}
+              {user && <NavMentor />}
+              <div className={`p-4 ${user ? 'sm:ml-72 pt-20' : ''} h-screen bg-gray-100`}>
+                <Breadcrumbs />
+                {!user ? <Login /> : <Dashboard />}
+              </div>
+            </>
+          }>
+          </Route>
 
-        {/* Mentor Route */}
-        <Route path='/mentor/dashboard' element={
-          <>
-            {user && <Sidebars />}
-            {user && <NavMentor />}
-            <div className={`p-4 ${user ? 'sm:ml-72 pt-20' : ''} h-screen bg-gray-100`}>
-              <Breadcrumbs />
-              {!user ? <Login /> : <Dashboard />}
-            </div>
-          </>
-        }>
-        </Route>
+          <Route path='/mentor/course' element={
+            <>
+              {user && <Sidebars />}
+              {user && <NavMentor />}
+              <div className={`p-4 ${user ? 'sm:ml-72 pt-20' : ''} h-screen bg-gray-100`}>
+                <Breadcrumbs />
+                {!user ? <Login /> : <Courses />}
+              </div>
+            </>
+          }>
+          </Route>
+          <Route path='/mentor/course/create' element={
+            <>
+              {user && <Sidebars />}
+              {user && <NavMentor />}
+              <div className={`p-4 ${user ? 'sm:ml-72 pt-20' : ''} h-screen bg-gray-100`}>
+                <Breadcrumbs />
+                {!user ? <Login /> : <CreateCourse />}
+              </div>
+            </>
+          }>
+          </Route>
 
-        <Route path='/mentor/course' element={
-          <>
-            {user && <Sidebars />}
-            {user && <NavMentor />}
-            <div className={`p-4 ${user ? 'sm:ml-72 pt-20' : ''} h-screen bg-gray-100`}>
-              <Breadcrumbs />
-              {!user ? <Login /> : <Courses />}
-            </div>
-          </>
-        }>
-        </Route>
-        <Route path='/mentor/course/create' element={
-          <>
-            {user && <Sidebars />}
-            {user && <NavMentor />}
-            <div className={`p-4 ${user ? 'sm:ml-72 pt-20' : ''} h-screen bg-gray-100`}>
-              <Breadcrumbs />
-              {!user ? <Login /> : <CreateCourse />}
-            </div>
-          </>
-        }>
-        </Route>
-
-        {/* Mentor Route */}
-        <Route path='*' element={<Nofound />}></Route>
-      </Routes>
+          {/* Mentor Route */}
+          <Route path='*' element={<Nofound />}></Route>
+        </Routes>
+      </Analytics>
     </div>
   );
 }
